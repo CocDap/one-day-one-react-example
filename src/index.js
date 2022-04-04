f2ca65cc7a5717d16117ce366d457bc78e71335c
CocDap <hodinhdung1997@gmail.com>
2022-04-04 23:21:20 +0700

    feat: component in react

--- src/index.js
+++ src/index.js
@@ -4,10 +4,55 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+function Header(){
+  return (
+    <header>
+        <nav className="nav">
+          <img src= "../logo192.png" />
+          <ul className = "nav-items">
+            <li> Price </li>
+            <li> About</li>
+            <li> Contact</li>
+
+          </ul>
+        </nav>
+    </header>
+  )
+}
+
+function Footer(){
+  return (
+    <footer>
+      <small>Copy Development</small>
+    </footer>
+  )
+}
+
+function MainContent(){
+  return (
+    <div>
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Hello</li>
+          <li>world</li>
+          <li>Ho</li>
+          <li>Dinh</li>
+          <li>Dung</li>
+        </ul>
+    </div>
+  )
+}
+function Page(){
+  return (
+    <div>
+        <Header/>
+        <MainContent/>
+        <Footer/>
+    </div>
+  )
+}
+ReactDOM.render(<Page />,
   document.getElementById('root')
 );
 
